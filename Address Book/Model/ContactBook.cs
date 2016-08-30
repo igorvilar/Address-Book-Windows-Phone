9733150f204bf113c87ff512f634515ac74581ea
IGOR VILAR <igorvilar@outlook.com>
2016-08-30 14:04:38 -0300

    Estruturado pastas e add model de contatos

---
+++ Address Book/Model/ContactBook.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book.Model
+{
+    class ContactBook
+    {
+        //The Id property is marked as the Primary Key
+        [SQLite.PrimaryKey, SQLite.AutoIncrement]
+        public int IdContact { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public ContactBook()
+        {
+            //empty constructor
+        }
+        public ContactBook(string name, string phone, string address)
+        {
+            Name = name;
+            Phone = phone;
+            Address = address;
+        }
+    }
+}
